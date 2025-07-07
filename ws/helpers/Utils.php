619a4b7dc57796aaa8ab01c40209bdeeae135d2d
Ratsito Mitia <140900716+RtsMitia@users.noreply.github.com>
2025-07-07 20:28:54 +0300

    commit

--- ws/helpers/Utils.php
+++ ws/helpers/Utils.php
@@ -5,4 +5,9 @@ class Utils {
         $dt = new DateTime($date);
         return $dt->format('d/m/Y');
     }
+
+    public static function anuiteConstante($capitalEmprunte, $tauxInteret, $totalMensualite) {
+        $tauxInteret = $tauxInteret/12;
+        return $capitalEmprunte * ($tauxInteret / (1 - pow(1 + $tauxInteret, -$totalMensualite)));
+    }
 }
