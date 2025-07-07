4f9e0ce907ad8c116a0dc158b0cdce4ddd6f1df3
Randie Ranaivoson <randieranaivoson@gmail.com>
2025-07-07 16:00:42 +0300

    model + controller

---
+++ ws/controllers/PretController.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/../models/Pret.php';
+require_once __DIR__ . '/../helpers/Client.php';
+require_once __DIR__ . '/../helpers/TypePret.php';
+
+
+
+class PretController {
+    public static function insertionPret()
+}
